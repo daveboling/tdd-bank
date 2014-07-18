645b0935a24ea76df06df048b91be9cd6d65a12f
Dave Boling <daviddboling@gmail.com>
2014-07-18 14:43:14 -0500

    Added final function, withdraw, program complete

--- app/models/account.js
+++ app/models/account.js
@@ -1,10 +1,12 @@
 'use strict';
 
 function Account(number, name, type, balance){
-  this.number  = number;
-  this.name    = name;
-  this.type    = type;
-  this.balance = balance;
+  this.number      = number;
+  this.name        = name;
+  this.type        = type;
+  this.balance     = balance;
+  this.isSuspended = false;
+  this.overdraft   = 0;
 
   this.deposits   = [];
   this.withdrawls = [];
@@ -12,6 +14,21 @@ function Account(number, name, type, balance){
 Account.prototype.deposit = function(amount){
   this.deposits.push(amount);
   this.balance += amount;
+};
+
+Account.prototype.withdraw = function(amount){
+  if(this.balance < 0 && this.overdraft === 3){
+    this.isSuspended = true;
+  }
+  else if(!this.isSuspended) {
+    this.withdrawls.push(amount);
+    this.balance -= amount;
+    if(this.balance < 0){
+      this.balance -= 50;
+      this.overdraft++;
+    }
+
+  }
 };
 
 
--- test/unit/account_test.js
+++ test/unit/account_test.js
@@ -16,7 +16,6 @@ describe('Account', function(){
       expect(acct1.number).to.equal(1234);
       expect(acct1.balance).to.equal(1500);
       expect(acct1.type).to.equal('savings');
-
     });
   });
   describe('#deposit', function() {
@@ -27,6 +26,35 @@ describe('Account', function(){
       expect(acct1.deposits[0]).to.equal(1000);
       expect(acct1.deposits).to.have.length(1);
       expect(acct1.balance).to.equal(2500);
+    });
+  });
+  describe('#withdrawl', function() {
+    it('should decrease balance and add 1 to withdrawl array', function(){
+      var acct1 = new Account(1234, 'bob', 'savings', 1500);
+      acct1.withdraw(500);
+
+      expect(acct1.withdrawls[0]).to.equal(500);
+      expect(acct1.balance).to.equal(1000);
+    });
+    it('should penalize acct holder for over draft', function(){
+      var acct1 = new Account(1234, 'bob', 'savings', 1500);
+      acct1.withdraw(5000);
+
+      expect(acct1.withdrawls[0]).to.equal(5000);
+      expect(acct1.balance).to.equal(-3550);
+      expect(acct1.overdraft).to.equal(1);
+    });
+    it('should suspend the acct holder if three withdrawls while overdrafted', function(){
+      var acct1 = new Account(1234, 'bob', 'savings', 1500);
+      acct1.withdraw(2000);
+      acct1.withdraw(1);
+      acct1.withdraw(1);
+      acct1.withdraw(1);
+      expect(acct1.withdrawls[0]).to.equal(2000);
+      expect(acct1.withdrawls[1]).to.equal(1);
+      expect(acct1.withdrawls[2]).to.equal(1);
+      expect(acct1.balance).to.equal(-652);
+      expect(acct1.isSuspended).to.equal(true);
     });
   });
 });
